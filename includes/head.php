0918e2f05823053b48a50ba8e9d34b555cef29c4
Shishik Julia <oiseua@gmail.com>
2017-06-16 06:11:12 +0300

    1

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon site</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script src="../js/jquery-3.2.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+      require_once 'core/init.php';
+      include "includes/head.php";
+      include "includes/navigations.php";
+      include "includes/headerfull.php";
+      include "includes/leftbar.php";
+      ?>
+
+    <!--Main content-->
+
+    <div class="col-md-8">
+    <div class="row">
+        <h2 class="text-center">Ассoртимент товара</h2>
+        <div class="col-md-3">
+            <h4>Levis Jeans</h4>
+            <img src="images/products/men4.png" alt="Levis Jeans"/>
+            <p class="list-price text-danger">Прайс-лист:<s>660.59</s></p>
+            <p class="price">Наша цена:499.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Levis Jeans</h4>
+            <img src="images/products/men4.png" alt="Levis Jeans"/>
+            <p class="list-price text-danger">Прайс-лист:<s>660.59</s></p>
+            <p class="price">Наша цена:499.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Levis Jeans</h4>
+            <img src="images/products/men4.png" alt="Levis Jeans"/>
+            <p class="list-price text-danger">Прайс-лист:<s>660.59</s></p>
+            <p class="price">Наша цена:499.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Levis Jeans</h4>
+            <img src="images/products/men4.png" alt="Levis Jeans"/>
+            <p class="list-price text-danger">Прайс-лист:<s>660.59</s></p>
+            <p class="price">Наша цена:499.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Holister shirt</h4>
+            <img src="images/products/men1.png" alt="Holister shirt"/>
+            <p class="list-price text-danger">Прайс-лист:<s>660.59</s></p>
+            <p class="price">Наша цена:499.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Fancy Shoes</h4>
+            <img src="images/products/women6.png" alt="Fancy Shoes"/>
+            <p class="list-price text-danger">Прайс-лист:<s>345.59</s></p>
+            <p class="price">Наша цена:299.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Levis Jeans</h4>
+            <img src="images/products/men4.png" alt="Levis Jeans"/>
+            <p class="list-price text-danger">Прайс-лист:<s>660.59</s></p>
+            <p class="price">Наша цена:499.00</p>
+            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Подробнее</button>
+        </div>
+   </div>
+</div>
+
+<?php
+include "includes/detailsmodal.php";
+include "includes/rightbar.php";
+include "includes/footer.php";
+?>
+
