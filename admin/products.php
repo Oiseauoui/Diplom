073847fb091b65bcfbe881c9056004dbc8cc2890
Shishik Julia <oiseua@gmail.com>
2017-06-18 06:51:21 +0300

    14

--- admin/products.php
+++ admin/products.php
@@ -14,7 +14,10 @@ if (isset($_GET['featured'])) {
 }
 
 ?>
-<h2 class="text-center">Продукция</h2><hr>
+<h2 class="text-center">Продукция</h2>
+    <a href="products.php?add=1" class="btn btn-success pull-right" id="add-product-btn">Добавить изделие</a>
+    <div class="clearfix"></div>
+    <hr>
 <table class="table table-bordered table-condensed table-striped">
     <thead>
     <th></th>
@@ -25,19 +28,30 @@ if (isset($_GET['featured'])) {
     <th>Продано</th>
     </thead>
     <tbody>
-    <?php while ($product = mysqli_fetch_assoc($presults)): ?>
-    <tr>
+    <?php while ($product = mysqli_fetch_assoc($presults)):
+        $childID = $product['categories'];
+        $catSql = "select * from categories where id = '$childID'";
+        $result = $db->query($catSql);
+        $child = mysqli_fetch_assoc($result);
+        $parentID = $child['parent'];
+        $pSql = "select * from categories where id = '$parentID'";
+        $presults = $db->query($pSql);
+        $parent = mysqli_fetch_assoc($presults);
+        $category = $parent['category'].'~'.$child['category'];
+        ?>
+
+        <tr>
         <td>
             <a href="products.php?edit=<?=$product['id']; ?>" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
             <a href="products.php?delete=<?=$product['id']; ?>" class="btn btn-default"><span class="glyphicon glyphicon-remove"></span></a>
         </td>
         <td><?=$product['title']; ?></td>
         <td><?=money($product['price']); ?></td>
-        <td></td>
+        <td><?=$category; ?></td>
         <td><a href="products.php?featured=<?=(($product['featured'] == 0)? '1' : '0'); ?>&id=<?=$product['id'];?>" class="btn btn-default">
-                <span class="glyphicon glyphicon-<?= (($product['featured'] == 1)? 'minus' : 'plus'); ?>"></span>
+           <span class="glyphicon glyphicon-<?= (($product['featured'] == 1)? 'minus' : 'plus'); ?>"></span>
             </a>&nbsp <?=(($product['featured'] ==  1)? 'изделие' : ''); ?></td>
-        <td></td>
+        <td>0</td>
     </tr>
     <?php endwhile; ?>
     </tbody>
